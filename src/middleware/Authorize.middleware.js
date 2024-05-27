d0db409d34940408084885a971b0e13d5e455013
arip995 <arindampanda2002@gmail.com>
2024-05-27 11:33:21 +0530

    Updated error handling

---
+++ src/middleware/Authorize.middleware.js
@@ -0,0 +1,24 @@
+import { sendResponse } from '../utils/Node.util.js';
+
+const secret = process.env.JWT_SECRET_KEY;
+
+export const authorizeMiddleware = (req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, secret, (err, decoded) => {
+      if (err) {
+        return sendResponse(
+          res,
+          401,
+          'Unauthorizes access'
+        );
+      }
+      req.body._id = decoded._id; // add decoded._id to req.body
+      next(); // call next middleware or route handler
+    });
+  } else {
+    return sendResponse(res, 401, 'Unauthorized access');
+  }
+};
--- src/routes/v1/v1.js
+++ src/routes/v1/v1.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
-import AuthRouter from './Auth.routes.js';
 import expressAsyncHandler from 'express-async-handler';
+import { errorHandlerMiddleware } from '../../middleware/errorHandler.middleware.js';
+import AuthRouter from './Auth.routes.js';
+
 import ImageRouter from './Image.routes.js';
 
 const v1Router = Router();
 
-v1Router.use('/auth', expressAsyncHandler(AuthRouter));
-v1Router.use('/image', expressAsyncHandler(ImageRouter));
+v1Router.use(
+  '/auth',
+  errorHandlerMiddleware,
+  expressAsyncHandler(AuthRouter)
+);
+v1Router.use(
+  '/image',
+  errorHandlerMiddleware,
+  expressAsyncHandler(ImageRouter)
+);
 
 export default v1Router;
